c2e471a422685a0b3741b2eee93cd0e6c57ba600
Musa Mhlambi <ST10498737@rcconnect.edu.za>
2026-09-09 01:50:11 +0200

    Refactor Welcome class and add audio playback
    Refactored the `welcome` method to return the user's name and made it, along with `DisplayLogo` and `recording`, publicly accessible. Added audio playback functionality to the `recording` method using the `SoundPlayer` class, with error handling for missing or inaccessible files. Included `System.Media` namespace for audio support.

--- Welcome.cs
+++ Welcome.cs
@@ -3,20 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Media;
 
 namespace CyberSecurity_Awareness_Chatbot
 {
     class Welcome
     {
-        static void welcome()
+        public static string welcome()
         {
             Console.WriteLine("Welcome to the Cyber Security Awareness Chatbot!");
             Console.WriteLine("Please enter your name: ");
             string userName = Console.ReadLine();
             Console.WriteLine($"Hello, {userName}! Let's get started with some cyber security awareness questions.");
+            return userName;
         }
 
-        static void DisplayLogo()
+        public static void DisplayLogo()
         {
             Console.WriteLine("  ____ _                 _   _                 _             _   _             ");
             Console.WriteLine(" / ___| | ___  _   _  __| | | |__   __ _ _ __ | |_ ___  __ _| |_(_) ___  _ __  ");
@@ -26,8 +28,17 @@ namespace CyberSecurity_Awareness_Chatbot
             Console.WriteLine();
         }
 
-        static void recording()
+        public static void recording()
         {
-            
+            try
+            {
+                SoundPlayer player = new SoundPlayer("Audio\\welcome.wav");
+                player.PlaySync();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Check file ");
+            }
         }
+    }
 }
