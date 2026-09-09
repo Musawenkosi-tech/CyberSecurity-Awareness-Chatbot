b54440919d05df9f5d03da8a3d65e8a80f06a5df
Musa Mhlambi <ST10498737@rcconnect.edu.za>
2026-09-09 02:43:30 +0200

    Add User class to manage user data in chatbot
    A new `User` class was introduced in the `CyberSecurity_Awareness_Chatbot` namespace. This class includes a `userName` property with `get` and `set` accessors and a constructor to initialize the property. Additionally, necessary `using` directives were added to the file to support the new functionality.

---
+++ User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CyberSecurity_Awareness_Chatbot
+{
+    public class User
+    {
+        public string userName {  get; set; }
+        public User( string userName) {
+            this.userName = userName;
+        }   
+    }
+}
